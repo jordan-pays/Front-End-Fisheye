4d8762d14fd9bf67060de182edefa8b1e4368dc7
pays jordan <jordan@lmksystem.fr>
2022-08-17 09:58:42 +0200

    affichage des photographes dans index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,25 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait,city,country,tagline,price,id } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("id", id)
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + ", " + country;
+        const h4 = document.createElement( 'h4' );
+        h4.textContent = tagline;
+        const h5 = document.createElement( 'h5' );
+        h5.textContent = price + "€/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
         return (article);
     }
     return { name, picture, getUserCardDOM }
